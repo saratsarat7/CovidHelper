a1e0bd537369f39fa624103c7608010867e6f818
rajsethi05 <raj.sethi05@gmail.com>
2021-05-08 15:37:38 +0400

    Update __init__.py
    converted datetime to IST

--- create_giver/__init__.py
+++ create_giver/__init__.py
@@ -1,5 +1,5 @@
 import logging
-
+import pytz
 from datetime import datetime
 import os
 import certifi
@@ -10,6 +10,7 @@ import azure.functions as func
 DB_NAME = os.environ["DB_NAME"]
 MONGO_CONNECTION = os.environ["MONGO_CONNECTION"]
 help_giver_collection = os.environ["help_giver_collection"]
+tz_NY = pytz.timezone('Asia/Kolkata')
 
 client = MongoClient(MONGO_CONNECTION, tlsCAFile=certifi.where())
 db = client[DB_NAME]
@@ -54,7 +55,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             request_body = filter_body(create_helper_request_body, req_body)
             request_body['helper_location']["longitude"] = round(request_body['helper_location']["longitude"], 6)
             request_body['helper_location']["lattitude"] = round(request_body['helper_location']["lattitude"], 6)
-            request_body['date_time'] = datetime.now()
+            datetime_NY = datetime.now(tz_NY)
+            date_time=datetime_NY.strftime("%Y-%m-%d %H:%M:%S")
+            request_body['date_time'] = date_time
             object_ID = help_giver.insert_one(request_body).inserted_id
             return func.HttpResponse(
                 f"Success {object_ID}",
